e5a91f796de678b51a4c773c5e155861681affed
phwt.smwt <phwt.smwt@gmail.com>
2021-11-06 00:46:06 +0700

    feat: repository for models

---
+++ src/02/Start_Here/MyShop.Infrastructure/Repositories/CustomerRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using MyShop.Domain.Models;
+
+namespace MyShop.Infrastructure.Repositories
+{
+    public class CustomerRepository : GenericRepository<Customer>
+    {
+        public CustomerRepository(ShoppingContext context) : base(context)
+        {
+        }
+
+        public override Customer Update(Customer entity)
+        {
+            var customer = context.Customers.Single(c => c.CustomerId == entity.CustomerId);
+
+            customer.Name = entity.Name;
+            customer.City = entity.City;
+            customer.PostalCode = entity.PostalCode;
+            customer.ShippingAddress = entity.ShippingAddress;
+            customer.Country = entity.Country;
+
+            return base.Update(customer);
+        }
+    }
+}
---
+++ src/02/Start_Here/MyShop.Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using MyShop.Domain.Models;
+
+namespace MyShop.Infrastructure.Repositories
+{
+    public class ProductRepository : GenericRepository<Product>
+    {
+        public ProductRepository(ShoppingContext context) : base(context)
+        {
+        }
+
+        public override Product Update(Product entity)
+        {
+            var product = context.Products.Single(p => p.ProductId == entity.ProductId);
+
+            // override method to limit the modification to these fields only
+            product.Name = entity.Name;
+            product.Price = entity.Price;
+
+            return base.Update(product);
+        }
+    }
+}
